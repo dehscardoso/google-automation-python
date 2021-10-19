229f5f844a422ea89475e39e88913df6166ccddf
Deborah <86526328+dehscardoso@users.noreply.github.com>
2021-10-19 11:52:52 -0300

    del

--- Crash_Course_of_Python/Week 1/ratio.py
+++
@@ -1,5 +0,0 @@
-# Replace the ___ placeholder and calculate the Golden ratio:  frac{1+\sqrt{5}}{2}
-# Tip: to calculate the square root of a number xx, you can use x**(1/2)
-
-ratio = (1 + (5** (1 / 2))) / 2
-print(ratio)
--- Crash_Course_of_Python/Week 3/show_letters.py
+++
@@ -1,9 +0,0 @@
-# The show_letters function should print out each letter of a word on a separate line. 
-# Fill in the blanks to make that happen.
-
-def show_letters(word):
-	for i in range (0,len(word)):
-		print(word[i])
-
-show_letters("Hello")
-# Should print one line per letter
